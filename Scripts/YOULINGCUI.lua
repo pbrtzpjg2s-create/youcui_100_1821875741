071b9f5c8912c038d28c2791143a71220640fcf4
pbrtzpjg2s-create <pbrtzpjg2s@privaterelay.appleid.com>
2025-12-12 20:45:19 +0800

    Add files via upload

---
+++ Scripts/YOULINGCUI.lua
@@ -0,0 +1,15 @@
+﻿local YOULING = GameMain:GetMod("YOULINGCUI");
+local time = 0;
+local flag = 0;
+
+
+function YOULING:OnStep(dt)
+if flag == 0 then
+time = time + dt;
+if time >= 10 then
+flag = 1;
+CS.XiaWorld.GameDefine.SOULCRYSTALYOU_BASE = 99999;
+CS.XiaWorld.GameDefine.SOULCRYSTALLING_BASE = 99999;
+end
+end
+end
